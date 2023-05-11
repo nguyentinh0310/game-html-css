daad040870641aa5e3f27f127f1dc87a9c43015f
nguyentinh0310 <nguyenquoctinh0310@gmail.com>
2023-05-11 18:03:39 +0700

    update style tournament

--- tournament/main.js
+++ tournament/main.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
-  var slickOptions = {
+  const slickOptions = {
     infinite: true,
     speed: 1000,
     slidesToShow: 3,
     slidesToScroll: 1,
     prevArrow:
-      "<button type='button' class='slick-prev' disabled>Previous</button>",
+      "<button type='button' class='slick-prev'>Previous</button>",
     nextArrow: "<button type='button' class='slick-next'>Next</button>",
     responsive: [
       {
@@ -19,8 +19,8 @@ $(document).ready(function () {
       {
         breakpoint: 768,
         settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
+          slidesToShow: 1,
+          slidesToScroll: 1,
         },
       },
       {
@@ -33,17 +33,52 @@ $(document).ready(function () {
     ],
   };
 
-  var slider = $(".slider").slick(slickOptions);
+  const slickTour = {
+    infinite: true,
+    speed: 1000,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    prevArrow:
+      "<button type='button' class='slick-prev'>Previous</button>",
+    nextArrow: "<button type='button' class='slick-next'>Next</button>",
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+        },
+      },
+    ],
+  };
 
-  if (slider.slick("slickCurrentSlide") === 0) {
-    $(".slick-prev").attr("disabled", true);
-  }
+  const slider = $("#slider-up-coming").slick(slickOptions);
+  $("#slider-on-going").slick(slickTour);
 
-  slider.on("afterChange", function (event, slick, currentSlide) {
-    if (currentSlide === 0) {
-      $(".slick-prev").attr("disabled", true);
-    } else {
-      $(".slick-prev").removeAttr("disabled");
-    }
-  });
+  // if (slider.slick("slickCurrentSlide") === 0) {
+  //   $(".slick-prev").attr("disabled", true);
+  // }
+
+  // slider.on("afterChange", function (event, slick, currentSlide) {
+  //   if (currentSlide === 0) {
+  //     $(".slick-prev").attr("disabled", true);
+  //   } else {
+  //     $(".slick-prev").removeAttr("disabled");
+  //   }
+  // });
+  $("#btn-more").click(function () {
+    $(".rank-prize").css("display", "block");
+  })
+  $("#btn-close").click(function () {
+    $(".rank-prize").css("display", "none");
+  })
 });
